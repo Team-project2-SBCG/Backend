4d9b8783d21b01d0604b7923ebe4b0ba6387ffdb
yuny0623 <overman522@gmail.com>
2022-03-18 21:32:15 +0900

    first commit

---
+++ src/main/java/com/booktree/booktreespring/BooktreeSpringApplication.java
@@ -0,0 +1,12 @@
+package com.booktree.booktreespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktreeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktreeSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/booktree/booktreespring/BooktreeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booktree.booktreespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktreeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
